4af148d9b3212267a3ba12ba8172eb1942889ca0
Surv <135030944+SurvExe1Pc@users.noreply.github.com>
2023-12-01 12:36:32 -0500

    Expose _StackFrame and Thread classes
    Useful for starting threads with data or creating fake threads

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -27,6 +27,7 @@ const {loadSound} = require('./import/load-sound.js');
 const {serializeSounds, serializeCostumes} = require('./serialization/serialize-assets');
 require('canvas-toBlob');
 const {exportCostume} = require('./serialization/tw-costume-import-export');
+const {Thread, _StackFrame} = require('./engine/thread.js');
 const Base64Util = require('./util/base64-util');
 
 const RESERVED_NAMES = ['_mouse_', '_stage_', '_edge_', '_myself_', '_random_'];
@@ -218,7 +219,9 @@ class VirtualMachine extends EventEmitter {
         this.exports = {
             Sprite,
             RenderedTarget,
-            JSZip
+            JSZip,
+            Thread,
+            _StackFrame
         };
     }
 
